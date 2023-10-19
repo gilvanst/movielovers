c0a8d962b73fabcf795f2d68742d1d2ef1f438af
Gilvan <gilvanjr306@gmail.com>
2023-10-19 10:55:43 -0300

    Finalizando index

--- index.php
+++ index.php
@@ -1,31 +1,68 @@
-<?php require_once "templates/header.php"; ?>
+<?php
+require_once "templates/header.php";
+require_once "dao/movieDAO.php";
 
-    <div id="main-container" class="container-fluid">
-        <h2 class="section-title">Filmes novos</h2>
-        <p class="section-description">Veja as Críticas dos últimos filmes adicionados</p>
-        <div class="movies-container"></div>
-        
-        <h2 class="section-title">Ação</h2>
-        <p class="section-description">Veja os Melhores filmes de ação</p>
-        <div class="movies-container">
-            <div class="card movie-card">
-                <div class="card-img-top" style="background-image: url('<?= $BASE_URL ?>/img/movies/movie_cover.jpg');"></div>
-                <div class="card-body">
-                    <p class="card-rating">
-                    <i class="fas fa-star"></i>
-                    <span class="rating">9</span>
-                    </p>
-                    <h5 class="card-title"><a href="#">Titulo do filme</a></h5>
-                    <a href="#" class="btn btn-primary rate-btn">Avaliar</a>
-                    <a href="#" class="btn btn-primary card-btn">Conhecer</a>
-                </div>
 
-            </div>
-        </div>
-        
-        <h2 class="section-title">Comédia</h2>
-        <p class="section-description">Veja os melhores filmes de comédia</p>
-        <div class="movies-container"></div>
+//DAO dos filmes
+$movieDao = new MovieDAO($conn, $BASE_URL);
+
+//Categorias de filmes
+$latestMovies = $movieDao->getLatestMovies();
+$actionMovies = $movieDao->getMoviesByCategorys("Ação");
+$comedyMovies = $movieDao->getMoviesByCategorys("Comédia");
+?>
+
+<div id="main-container" class="container-fluid">
+    <h2 class="section-title">Filmes novos</h2>
+    <p class="section-description">Veja as Críticas dos últimos filmes adicionados</p>
+    <div class="movies-container">
+
+        <?php foreach ($latestMovies as $movies) : ?>
+
+            <?php require "templates/movie_card.php"; ?>
+
+        <?php endforeach; ?>
+
+        <?php if (count($latestMovies) === 0) : ?>
+
+            <p class="empty-list">Ainda não há filmes cadastrados!</p>
+
+        <?php endif; ?>
     </div>
+
+    <h2 class="section-title">Comédia</h2>
+    <p class="section-description">Veja os melhores filmes de comédia</p>
+    <div class="movies-container">
+
+        <?php foreach ($comedyMovies as $movies) : ?>
+
+            <?php require "templates/movie_card.php"; ?>
+
+        <?php endforeach; ?>
+
+        <?php if (count($comedyMovies) === 0) : ?>
+
+            <p class="empty-list">Ainda não há filmes cadastrados!</p>
+
+        <?php endif; ?>
+    </div>
+
+    <h2 class="section-title">Ação</h2>
+    <p class="section-description">Veja os melhores filmes de comédia</p>
+    <div class="movies-container">
+        
+        <?php foreach ($actionMovies as $movies) : ?>
+
+            <?php require "templates/movie_card.php"; ?>
+
+        <?php endforeach; ?>
+
+        <?php if (count($actionMovies) === 0) : ?>
+
+            <p class="empty-list">Ainda não há filmes cadastrados!</p>
+
+        <?php endif; ?>
+    </div>
+</div>
 
 <?php require_once "templates/footer.php"; ?>
